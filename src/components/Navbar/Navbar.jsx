568c57659f7ad3694fe1a931c68b05f6b77cbe2c
Rawle Juglal <rawledjuglal@gmail.com>
2022-11-16 19:04:38 -0600

    Update Navbar.jsx
    changed path to troll icon

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import './Navbar.css';
-import Troll from '/MemeGenerator/images/TrollFace.png'
 
 export default function Navbar(){
     return(
         <div className='--navbar-navbar-container'>
             <div className="--navbar-logo-container">
-                <img className='--navbar-logo' src={Troll} />
+                <img className='--navbar-logo' src='/MemeGenerator/images/TrollFace.png' />
                 <p className='--navbar-logo-title'>Meme Generator</p>
             </div>
             <div className='--navbar-desc-container'>
